a38f84252dcd0f665d6ee196b48df7646f2a260a
William Custodio <wcustodio@atlassian.com>
2020-09-18 15:08:48 +0100

    style: Reorder the requirements on module-info.java

--- usecase/src/java/module-info.java
+++ usecase/src/java/module-info.java
@@ -3,9 +3,9 @@ module email.information.extractor.usecase {
     requires com.fasterxml.jackson.annotation;
     requires commons.io;
     requires mail;
+    requires email.information.extractor.domain;
     requires lombok;
     requires spring.beans;
     requires spring.context;
     requires org.apache.pdfbox;
-    requires email.information.extractor.domain;
 }
